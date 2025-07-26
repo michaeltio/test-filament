2061cf2815cf0a3bc80a62b39ef4e310b55bcfe9
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-07-26 09:57:21 +0700

    first commit

---
+++ app/Filament/Resources/BarangResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BarangResource\Pages;
+use App\Filament\Resources\BarangResource\RelationManagers;
+use App\Models\Barang;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BarangResource extends Resource
+{
+    protected static ?string $model = Barang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama_barang'),
+                TextInput::make('kode_barang'),
+                TextInput::make('harga_barang'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_barang'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBarangs::route('/'),
+            'create' => Pages\CreateBarang::route('/create'),
+            'edit' => Pages\EditBarang::route('/{record}/edit'),
+        ];
+    }
+}
